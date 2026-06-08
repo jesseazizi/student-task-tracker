267b6194eb4352e8cc32c80f1021116c365dbf5d
Joseph Chavez <rommelchavez99@gmail.com>
2026-06-08 00:54:44 -0400

    fix: recreate project as Maven Spring Boot app

---
+++ src/main/java/com/example/studenttasktracker/StudentTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.studenttasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTaskTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studenttasktracker/StudentTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studenttasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
